a8d8d422447e7f711dd06756f5b83bdec6fa10da
LeriAx75 <a01663303@tec.mx>
2025-03-20 13:13:55 -0600

    Método rectangle() para dibujar un rectángulo agregado

--- paint.py
+++ paint.py
@@ -43,7 +43,20 @@ def circle(start, end):
 
 def rectangle(start, end):
     """Draw rectangle from start to end."""
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+    width = abs(end.x - start.x)
+    height = abs(end.y - start.y)
+
+    for count in range(2):
+        forward(width)
+        left(90)
+        forward(height)
+        left(90)
+
+    end:fill()
 
 
 def triangle(start, end):
